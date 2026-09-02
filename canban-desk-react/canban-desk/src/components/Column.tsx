e480cefb5351f88630df45cc87eb0d89c5aaa1c0
Egor Meshakshin <sdsmehakhin76@gmail.com>
2026-09-02 22:05:23 +0500

    add logic about drag and drop

--- canban-desk-react/canban-desk/src/components/Column.tsx
+++ canban-desk-react/canban-desk/src/components/Column.tsx
@@ -1,6 +1,13 @@
+import type { Task } from "../types/types";
+type ColumnProps = {
+    title:string;
+    tasks:Task[];
+    onDragOver:(e:React.DragEvent<HTMLDivElement>)=>void;
+    onDrop:(e:React.DragEvent<HTMLDivElement>)=>void;
+}
 
 function Column(){
-    
+
     return(
         <>
         
